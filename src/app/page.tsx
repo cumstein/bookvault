376575efc94f32243da434792efdc1e7046c8e38
Cumstein <kamyabaghaz@gmail.com>
2025-05-27 20:50:41 +0330

    done with configs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
-export default function Home( )  {   // extra spaces
+export default function Home() {
   return (
     <div className="h-screen flex justify-center items-center">
       <h1 className="justify-center items-center">Book Vault</h1>
     </div>
-  )
-} // missing semicolon
+  );
+}
