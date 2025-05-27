a44665d9096c9b9bf8735419109ee2e6f133efff
Cumstein <kamyabaghaz@gmail.com>
2025-05-27 20:46:43 +0330

    testing prettier

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  "**/*.{js,ts,tsx}": ["prettier --write"],
+  '**/*.{js,ts,tsx}': ['prettier --write'],
 };
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
-export default function Home() {
+export default function Home( )  {   // extra spaces
   return (
     <div className="h-screen flex justify-center items-center">
       <h1 className="justify-center items-center">Book Vault</h1>
     </div>
-  );
-}
+  )
+} // missing semicolon
