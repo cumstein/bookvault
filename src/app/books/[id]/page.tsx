dad4bb4d1ae6028f42c52a443b67bbac4030314c
Cumstein <kamyabaghaz@gmail.com>
2025-05-29 18:36:18 +0330

    [id] pages added

--- src/app/books/[id]/page.tsx
+++ src/app/books/[id]/page.tsx
@@ -2,8 +2,8 @@ import BookDetails from '@/components/books/BookDetails';
 import Link from 'next/link';
 import { ArrowLeft } from 'lucide-react';
 
-export default async function BookPage({ params }: { params: { id: string } }) {
-  const { id } = await params;
+export default function BookPage({ params }: { params: { id: string } }) {
+  const { id } = params;
   return (
     <div className="p-6">
       <Link
